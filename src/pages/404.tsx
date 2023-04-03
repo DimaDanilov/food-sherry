10e6cdb269da64fd4b15571c89cadc794e174899
Dima Danilov <dima.danilov.2012@mail.ru>
2023-04-03 14:25:12 +0300

    add: 404 custom page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,53 @@
 import Layout from "@/components/layout/Layout";
+import { FONT_SIZE } from "@/styles/globalStyles";
+import Button from "@/ui/Button";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  const onGoHome = () => {
+    router.push("/");
+  };
+
   return (
     <Layout pageTitle="Not found" pageDescription="Something went wrong.">
-      <h1>NOT FOUND</h1>;
+      <Container>
+        <Image src="/icons/logo_color.svg" width={129} height={179} alt="" />
+        <PageInfo>
+          <Title>404</Title>
+          <p>Looks like you missed a page!</p>
+          <Button
+            fontSize={FONT_SIZE.p}
+            fontWeight="bold"
+            padding="10px"
+            styleType="primary"
+            value="Go_home"
+            onClick={onGoHome}
+          >
+            Go Home
+          </Button>
+        </PageInfo>
+      </Container>
     </Layout>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  width: 50%;
+  margin: 0 auto;
+  padding: 10% 0;
+`;
+const PageInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const Title = styled.h1`
+  font-size: 50px;
+`;
