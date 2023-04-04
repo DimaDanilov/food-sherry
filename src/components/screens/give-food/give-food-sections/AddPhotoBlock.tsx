2c2405bec5666c4f66b2d99c62d5d9518f8df2e1
Dima Danilov <dima.danilov.2012@mail.ru>
2023-04-04 13:01:20 +0300

    add: adding images feature (needs to fix bugs)

--- src/components/screens/give-food/give-food-sections/AddPhotoBlock.tsx
+++ src/components/screens/give-food/give-food-sections/AddPhotoBlock.tsx
@@ -1,3 +1,61 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
 export default function AddPhotoBlock() {
-  return <h1>Photos block</h1>;
+  const [photoArray, setPhotoArray] = useState<Array<File>>([]);
+  let photosElements;
+
+  useEffect(() => {
+    photosElements = photoArray.map((photo) => {
+      console.log(photo);
+      return (
+        <img
+          alt={photo.name}
+          width={"250px"}
+          height={"250px"}
+          src={URL.createObjectURL(photo)}
+        />
+      );
+    });
+  }, [photoArray.length]);
+
+  return (
+    <div>
+      <input
+        type="file"
+        name="addImage"
+        accept="image/png, image/gif, image/jpeg"
+        multiple
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          if (event.target.files) {
+            let newPhotos: Array<File> = [];
+            for (let i = 0; i < event.target.files.length; i++) {
+              newPhotos.push(event.target.files[i]);
+            }
+            setPhotoArray([...photoArray, ...newPhotos]);
+          }
+        }}
+      />
+      <PhotosContainer>{photosElements}</PhotosContainer>
+    </div>
+  );
 }
+
+{
+  /* {selectedImage && (
+  <div>
+    <img
+      alt="not found"
+      width={"250px"}
+      src={URL.createObjectURL(selectedImage[selectedImage.length - 1])}
+    />
+    <br />
+    <button onClick={() => setSelectedImage([])}>Remove</button>
+  </div>
+)} */
+}
+
+const PhotosContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
