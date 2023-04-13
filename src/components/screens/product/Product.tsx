cd0c35a2e3e26c868a98feb7202245847b4aa2d9
Dima Danilov <dima.danilov.2012@mail.ru>
2023-04-13 16:59:26 +0300

    restyled product page

--- src/components/screens/product/Product.tsx
+++ src/components/screens/product/Product.tsx
@@ -6,6 +6,7 @@ import { IconWithText } from "@/ui/IconWithText";
 import { parseCreateDate } from "@/utils/parseCreateDate";
 import { parseTakeDate } from "@/utils/parseTakeDate";
 import Image from "next/image";
+import { useState, useCallback, memo } from "react";
 import {
   HiOutlineUser,
   HiOutlineCake,
@@ -16,31 +17,78 @@ import {
 } from "react-icons/hi2";
 import styled from "styled-components";
 
-export default function ProductScreen({ product }: { product: FoodItem }) {
-  const handleClick = () => console.log("click");
+interface IProductImageProps {
+  imageUrl: string;
+  index: number;
+  currentImageID: number;
+  onImageClick: (id: number) => void;
+}
+interface IProductScreenProps {
+  product: FoodItem;
+}
 
-  const imageElements = product.imagesSrc.map((imageUrl, index) => {
+interface IRegularTextProps {
+  fontColor?: string;
+}
+interface IProductImageSmallProps {
+  border: string;
+  cursor: string;
+}
+
+const ProductImage = memo(
+  ({ imageUrl, index, currentImageID, onImageClick }: IProductImageProps) => {
+    const border =
+      index === currentImageID ? `6px double ${COLORS.mainColor}` : "none";
+    const cursor = index === currentImageID ? "default" : "pointer";
+
     return (
-      <Image
-        key={index}
+      <ProductImageSmall
         alt=""
         src={imageUrl || "/icons/product_placeholder.svg"}
-        width={150}
-        height={150}
+        width={100}
+        height={100}
+        onClick={() => onImageClick(index)}
+        border={border}
+        cursor={cursor}
       />
     );
-  });
+  }
+);
+
+export default function ProductScreen({ product }: IProductScreenProps) {
+  const handleClick = () => console.log("click");
+
+  const [currentImageID, setCurrentImageID] = useState<number>(0);
+
+  const onImageClick = useCallback((id: number) => {
+    setCurrentImageID(id);
+  }, []);
 
   return (
     <FlexContainer>
       <FlexItem>
-        <ProductImage
-          alt=""
-          src={product.imagesSrc[0] || "/icons/product_placeholder.svg"}
-          width={550}
-          height={550}
-        />
-        <GridImages>{imageElements}</GridImages>
+        <ProductImagesContainer>
+          <ProductImageBig
+            alt=""
+            src={
+              product.imagesSrc[currentImageID] ||
+              "/icons/product_placeholder.svg"
+            }
+            width={400}
+            height={400}
+          />
+          <GridImages>
+            {product.imagesSrc.map((imageUrl, index) => (
+              <ProductImage
+                key={index}
+                imageUrl={imageUrl}
+                index={index}
+                currentImageID={currentImageID}
+                onImageClick={onImageClick}
+              />
+            ))}
+          </GridImages>
+        </ProductImagesContainer>
       </FlexItem>
       <FlexItem>
         <h1>{product.title}</h1>
@@ -81,13 +129,14 @@ export default function ProductScreen({ product }: { product: FoodItem }) {
   );
 }
 
-interface IRegularTextProps {
-  fontColor?: string;
-}
-
 const FlexContainer = styled(Container)`
   display: flex;
   justify-content: space-between;
+`;
+const ProductImagesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 const FlexItem = styled.div`
   width: 45%;
@@ -99,11 +148,15 @@ const GridImages = styled.div`
   justify-items: center;
   gap: 10px;
 `;
-const ProductImage = styled(Image)`
+const ProductImageBig = styled(Image)`
+  width: 100%;
+`;
+const ProductImageSmall = styled(Image)<IProductImageSmallProps>`
   width: 100%;
   height: 100%;
+  border: ${(props) => props.border};
+  cursor: ${(props) => props.cursor};
 `;
-
 const RegularText = styled.p<IRegularTextProps>`
   margin: 10px auto;
   color: ${(props) => props.fontColor};
