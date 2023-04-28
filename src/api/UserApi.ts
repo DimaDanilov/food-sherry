846b5addc86b3f598b924673eb5aa25980f7534a
Dima Danilov <dima.danilov.2012@mail.ru>
2023-04-28 19:03:44 +0300

    add: update user feature

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -8,3 +8,38 @@ export async function loadOneUser(userId: string): Promise<IUser> {
   const response = await axios.get<IUser>(`${API_URL}/api/user/${userId}`);
   return UserAdapter.transform(response.data);
 }
+export async function updateUser({
+  userId,
+  name,
+  surname,
+  companyName,
+  phone,
+  email,
+}: {
+  userId: number;
+  name?: string;
+  surname?: string;
+  companyName?: string;
+  phone?: string;
+  email?: string;
+}): Promise<IUser> {
+  const token = localStorage.getItem("token");
+  const response = await axios.put(
+    `${API_URL}/api/user`,
+    {
+      id: userId,
+      name,
+      surname,
+      company_name: companyName,
+      phone,
+      email,
+    },
+    {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data;
+}
--- src/components/screens/profile/profile-sections/ProfileInfo.tsx
+++ src/components/screens/profile/profile-sections/ProfileInfo.tsx
@@ -1,7 +1,11 @@
 import { IUser } from "@/models/User";
 import { COLORS } from "@/styles/globalStyles";
+import { useState, useEffect } from "react";
 import { HiUserCircle } from "react-icons/hi2";
 import styled from "styled-components";
+import { updateUser } from "@/api/UserApi";
+import { useAuthStore } from "@/store/AuthStore";
+import Button from "@/ui/Button";
 
 interface ProfileScreenProps {
   user: IUser;
@@ -9,37 +13,177 @@ interface ProfileScreenProps {
 }
 
 export const ProfileInfo = ({ user, totalProducts }: ProfileScreenProps) => {
+  const authStore = useAuthStore();
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [phone, setPhone] = useState<string>(user.phone);
+  const [email, setEmail] = useState<string>(user.email);
+  const [name, setName] = useState<string>(user.name);
+  const [surname, setSurname] = useState<string>(user.surname);
+  const [companyName, setCompanyName] = useState<string>(user.companyName);
+
+  const onEditClick = async () => {
+    try {
+      if (isEditMode) {
+        await updateUser({
+          userId: user.id,
+          name,
+          surname,
+          companyName,
+          phone,
+          email,
+        });
+      }
+      setIsEditMode(!isEditMode);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onPhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+  };
+  const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const onSurnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSurname(e.target.value);
+  };
+  const onCompanyNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompanyName(e.target.value);
+  };
+
+  useEffect(() => {
+    setIsEditMode(false);
+    setPhone(user.phone);
+    setEmail(user.email);
+    setName(user.name);
+    setSurname(user.surname);
+    setCompanyName(user.companyName);
+  }, [user]);
+
   return (
     <div>
       <ProfileBriefData>
         <HiUserCircle fontSize={200} width="10px" color={COLORS.mainColor} />
-        <Title>
-          {user.name} {user.surname} {user.companyName}
-        </Title>
+
+        {user.name && isEditMode ? (
+          <>
+            <Input
+              autoFocus={true}
+              type="name"
+              id="name"
+              name="name"
+              value={name}
+              onChange={onNameChange}
+            />
+            <Input
+              type="surname"
+              id="surname"
+              name="surname"
+              value={surname}
+              onChange={onSurnameChange}
+            />
+          </>
+        ) : (
+          <>
+            <Title>{name}</Title>
+            <Title>{surname}</Title>
+          </>
+        )}
+
+        {user.companyName && isEditMode ? (
+          <Input
+            autoFocus={true}
+            type="companyName"
+            id="companyName"
+            name="companyName"
+            value={companyName}
+            onChange={onCompanyNameChange}
+          />
+        ) : (
+          <Title>{companyName}</Title>
+        )}
       </ProfileBriefData>
-      <InfoDetails>Телефон: {user.phone}</InfoDetails>
-      <InfoDetails>Почта: {user.email}</InfoDetails>
+
+      <InfoDetails>
+        <h5>Телефон:</h5>
+        {isEditMode ? (
+          <Input
+            type="phone"
+            id="phone"
+            name="phone"
+            value={phone}
+            onChange={onPhoneChange}
+          />
+        ) : (
+          <span>{phone}</span>
+        )}
+      </InfoDetails>
+
+      <InfoDetails>
+        <h5>Почта:</h5>
+        {isEditMode ? (
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={onEmailChange}
+          />
+        ) : (
+          <span>{email}</span>
+        )}
+      </InfoDetails>
+
       {user.timeCreated && (
+        <>
+          <InfoDetails>
+            <h5>Помогает с:</h5>
+            <span>{new Date(user.timeCreated).toLocaleDateString()}</span>
+          </InfoDetails>
+        </>
+      )}
+
+      {!Number.isNaN(totalProducts) && (
         <InfoDetails>
-          Помогает с: {new Date(user.timeCreated).toLocaleDateString()}
+          <h5>Создал объявлений:</h5>
+          <span>{totalProducts}</span>
         </InfoDetails>
       )}
-      {!Number.isNaN(totalProducts) && (
-        <InfoDetails>Создал объявлений: {totalProducts}</InfoDetails>
+
+      {user.id === authStore.user.id && (
+        <Button onClick={onEditClick} styleType="primary" padding="8px 0">
+          {isEditMode ? "Сохранить редактирование" : "Редактировать"}
+        </Button>
       )}
     </div>
   );
 };
 
 const ProfileBriefData = styled.div`
-  width: 80%;
   margin: 0 auto 20px;
   text-align: center;
 `;
 const Title = styled.h1`
   color: ${COLORS.mainColor};
 `;
-const InfoDetails = styled.h5`
-  color: ${COLORS.mainColor};
+const InfoDetails = styled.div`
+  & > h5 {
+    color: ${COLORS.mainColor};
+  }
+  margin: 10px auto;
+`;
+const Input = styled.input`
+  transition: 0.2s ease-in;
+  width: 100%;
   margin: 5px auto;
+  border-radius: 5px;
+  border: 1px solid ${COLORS.mainColor};
+  padding: 7px;
+  &:focus {
+    outline-color: ${COLORS.mainColor};
+  }
 `;
